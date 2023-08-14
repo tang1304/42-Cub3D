5c9e3fd6335d31c4e8d24b290123d6053ad7170f
Tanguy Gellon <tgellon@z4r11p1.42lyon.fr>
2023-08-14 15:40:22 +0200

    debut recup donnees .cub

--- Incs/cub3D.h
+++ Incs/cub3D.h
@@ -6,7 +6,7 @@
 /*   By: tgellon <tgellon@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/04 09:17:33 by tgellon           #+#    #+#             */
-/*   Updated: 2023/08/14 10:31:16 by tgellon          ###   ########lyon.fr   */
+/*   Updated: 2023/08/14 14:24:41 by tgellon          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,38 +17,61 @@
 # include <unistd.h>
 # include <limits.h>
 # include <stdlib.h>
+# include <fcntl.h>
 
 # define ESC 65307
 # define W 119
 # define A 97
 # define S 115
 # define D 100
+# define LEFT 68
+# define RIGHT 67
 # define RED_CROSS 33
 
 typedef struct s_img
 {
 	void	*img;
-	char	*addr; //img address
-	int		bpp; //bits per pixel
-	int		endian; //the way bytes are stored
-	int		line_l; //line length
+	char	*addr;//img address
+	int		bpp;//bits per pixel
+	int		endian;//the way bytes are stored
+	int		line_l;//line length
 }			t_img;
+
+typedef struct s_texture
+{
+	void	*text;
+	char	*addr;//img address
+	int		bpp;//bits per pixel
+	int		endian;//the way bytes are stored
+	int		line_l;//line length
+	int		height;
+	int		width;
+}			t_texture;
 
 typedef struct s_map
 {
-	int		p_x;
-	int		p_y;
-	char	**map;
-	int		width;
-	int		height;
-	int		p;
+	int			p_x;//player position on x
+	int			p_y;//player position on y
+	char		direction;//player orientation
+	char		**tmp;//content of .cub file
+	char		**map;
+	t_texture	*no;
+	t_texture	*so;
+	t_texture	*ea;
+	t_texture	*we;
+	int			f;//floor color
+	int			c;//ceiling color
+	// int		width;
+	// int		height;
 }			t_map;
 
 typedef struct s_data
 {
-	void	*mlx; //mlx pointer
-	void	*win; //window pointer
+	void	*mlx;//mlx pointer
+	void	*win;//window pointer
 	t_img	*img;
+	t_map	*map;
+	
 }			t_data;
 
 /*	close.c	*/
