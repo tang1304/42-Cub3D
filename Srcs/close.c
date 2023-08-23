66c80dbc0e5378755a882ad08f78deb58977d6c6
Tanguy Gellon <tgellon@z4r10p5.42lyon.fr>
2023-08-23 11:40:43 +0200

    merge clean

--- Srcs/close.c
+++ Srcs/close.c
@@ -6,7 +6,7 @@
 /*   By: tgellon <tgellon@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/14 09:58:17 by tgellon           #+#    #+#             */
-/*   Updated: 2023/08/16 09:54:50 by tgellon          ###   ########lyon.fr   */
+/*   Updated: 2023/08/23 11:39:50 by tgellon          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,3 +35,12 @@ void	close_map_error(t_data *data)
 // 	ft_free_maps(data);
 // 	exit(EXIT_SUCCESS);
 // }
+
+int	ft_close(t_data *data)
+{
+	mlx_destroy_window(data->mlx, data->win);
+	mlx_destroy_display(data->mlx);
+	free(data->mlx);
+	// ft_free_maps(data);
+	exit(EXIT_SUCCESS);
+}
