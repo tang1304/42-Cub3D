3663dfd95798a86c9f358e2d750aec6cec3b0d1a
Rolando Rebois <rrebois@z3r3p3.42lyon.fr>
2023-08-23 08:00:02 +0200

    minos changes

--- Incs/cub3D.h
+++ Incs/cub3D.h
@@ -6,7 +6,7 @@
 /*   By: rrebois <rrebois@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/04 09:17:33 by tgellon           #+#    #+#             */
-/*   Updated: 2023/08/21 14:51:39 by rrebois          ###   ########lyon.fr   */
+/*   Updated: 2023/08/21 16:46:24 by rrebois          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -78,6 +78,7 @@ typedef struct s_data
 	void	*win; //window pointer
 	int		win_h;
 	int		win_l;
+	float	view_d;
 	t_map	map;
 	t_col	col;
 	t_img	img;
