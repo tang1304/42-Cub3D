ced3aec03ba32c5cf059a91b2cdbfa5347bd1d7f
tang1304 <tanguy.gellon@gmail.com>
2023-09-04 08:51:44 +0200

    fix des segfault sur hooks_changes

--- Srcs/hooks_changes.c
+++ Srcs/hooks_changes.c
@@ -6,7 +6,7 @@
 /*   By: tgellon <tgellon@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/16 10:41:04 by rrebois           #+#    #+#             */
-/*   Updated: 2023/08/31 17:27:18 by tgellon          ###   ########lyon.fr   */
+/*   Updated: 2023/09/04 08:47:29 by tgellon          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,15 @@ void	change_board(t_data *data, int keycode)
 {
 	int	x;
 	int	y;
-	// int	m_x;
-	// int	m_y;
-
-	x = (data->win_w / 2) - (data->mini.width / 2);
-	y = (data->win_h / 2) - (data->mini.height / 2);
 	mlx_mouse_get_pos(data->mlx, data->win, &x, &y);
-	// m_x = x / data->square_size;
-	// m_y = y / data->square_size;
-	if (x < 0 || y < 0 || x > data->map.width || y > data->map.height)
+	x /= data->square_size;
+	y /= data->square_size;
+	if (x < 0 || y < 0 || x > data->mini.width || y > data->mini.height)
 		return ;
 	if (keycode == Z)
-		data->arr[y][x] = 0;
+		data->arr[y][x] = '0';
 	else if (keycode == X)
-		data->arr[y][x] = 1;
+		data->arr[y][x] = '1';
 	create_board_img(data);
 	mlx_put_image_to_window(data->mlx, data->win, data->img.img, 0, 0);
 }
--- Srcs/init_array_map.c
+++ Srcs/init_array_map.c
@@ -6,13 +6,13 @@
 /*   By: tgellon <tgellon@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/16 11:34:53 by rrebois           #+#    #+#             */
-/*   Updated: 2023/08/31 16:56:28 by tgellon          ###   ########lyon.fr   */
+/*   Updated: 2023/09/04 08:17:06 by tgellon          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../Incs/cub3D.h"
 
-static void	create_cpy_map_arr(t_data *data)
+void	create_cpy_map_arr(t_data *data)
 {
 	int	i;
 	int	j;
@@ -41,24 +41,3 @@ printf("len:%zu\n", ft_strlen(data->map.map[i]));
 		}
 	}
 }
-
-void	init_map(t_data *data)
-{
-	int	i;
-	int	j;
-
-	i = -1;
-	while (++i < data->map.height)
-	{
-		j = -1;
-		while (++j < data->map.width)
-		{
-			if (i == 0 || i == data->map.height - 1 || j == 0 || j == data->map.width - 1)
-				data->map.map[i][j] = 1;
-			else
-				data->map.map[i][j] = 0;
-		}
-	}
-	create_cpy_map_arr(data);
-}
-
