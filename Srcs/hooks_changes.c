c84815a1d1c34cd052d6cbd53410cd6d17e3c857
Rolando Rebois <rrebois@z3r3p3.42lyon.fr>
2023-09-08 10:21:52 +0200

    implemented forward and backward

--- Srcs/hooks_changes.c
+++ Srcs/hooks_changes.c
@@ -6,7 +6,7 @@
 /*   By: rrebois <rrebois@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/16 10:41:04 by rrebois           #+#    #+#             */
-/*   Updated: 2023/09/08 09:30:57 by rrebois          ###   ########lyon.fr   */
+/*   Updated: 2023/09/08 10:20:40 by rrebois          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,60 +32,72 @@ void	change_board(t_data *data, int keycode)
 
 static void	player_dst_pos(t_data *data, int keycode)
 {
-// printf("angle initial: %f\n", data->player.angle * 180 / M_PI);
+	double	old_dir_x;
+
+	old_dir_x = data->player.dir.x;
 	if (keycode == A)
+	{
 		data->player.angle += M_PI_4;
+		data->player.dir.x = old_dir_x * cos(-M_PI_4) - \
+							data->player.dir.y * sin(-M_PI_4);
+		data->player.dir.y = old_dir_x * sin(-M_PI_4) + \
+							data->player.dir.y * cos(-M_PI_4);
+	}
 	else if (keycode == D)
+	{
 		data->player.angle -= M_PI_4;
+		data->player.dir.x = old_dir_x * cos(M_PI_4) - \
+							data->player.dir.y * sin(M_PI_4);
+		data->player.dir.y = old_dir_x * sin(M_PI_4) + \
+							data->player.dir.y * cos(M_PI_4);
+	}
 	if (data->player.angle > 2 * M_PI)
 	data->player.angle -= 2 * M_PI;
 	else if (data->player.angle < -(2 * M_PI))
 		data->player.angle += 2 * M_PI;
-	data->player.view_dst_pos.x = data->square_view_d * \
-			cos(-data->player.angle) + data->player.pos.x;
-	data->player.view_dst_pos.y = data->square_view_d * \
-			sin(-data->player.angle) + data->player.pos.y;
-// printf("x: %d\n", data->player.view_dst_pos.x);
-// printf("y: %d\n", data->player.view_dst_pos.y);
+	// data->player.view_dst_pos.x = data->square_view_d * \
+	// 		cos(-data->player.angle) + data->player.pos.x;
+	// data->player.view_dst_pos.y = data->square_view_d * \
+	// 		sin(-data->player.angle) + data->player.pos.y;
 }
 
 void	rotate(t_data *data, int keycode)
-{(void)keycode;
-	// double		rot_speed;
-	// double		old_dir_x;
-	// t_coord_f	dest;
-
+{
 	player_dst_pos(data, keycode);
 	create_rays(data, data->player.view_dst_pos, data->player.angle);
-	// else
-
-
 }
 
-// void	move_forward(t_data *data)
-// {
-// 	double		move_speed;
-// 	t_coord_d	new_pos;
+void	move_forward(t_data *data)
+{
+	double		move_speed;
+	t_coord_d	new_pos;
 
-// 	move_speed = 10;
-// 	new_pos.x = data->player.dir.x * move_speed;
-// 	new_pos.y = data->player.dir.y * move_speed;
+	move_speed = 10;
+	new_pos.x = data->player.dir.x * move_speed;
+	new_pos.y = data->player.dir.y * move_speed;
 
-// 	data->player.pos.x += new_pos.x;
-// 	data->player.pos.y += new_pos.y;
-// 	create_rays(data, data->player.dir);
-// }
+	data->player.pos.x += new_pos.x;
+	data->player.pos.y += new_pos.y;
+	create_board_img(data);
+	create_cone_multi_rays(data, data->player.angle);
+	// create_rays(data, data->player.view_dst_pos, data->player.angle);
+}
 
-// void	move_backward(t_data *data)
-// {
-// 	double		move_speed;
-// 	t_coord_d	new_pos;
+void	move_backward(t_data *data)
+{
+	double		move_speed;
+	t_coord_d	new_pos;
 
-// 	move_speed = -10;
-// 	new_pos.x = data->player.dir.x * move_speed;
-// 	new_pos.y = data->player.dir.y * move_speed;
+	move_speed = -10;
+	new_pos.x = data->player.dir.x * move_speed;
+	new_pos.y = data->player.dir.y * move_speed;
 
-// 	data->player.pos.x += new_pos.x;
-// 	data->player.pos.y += new_pos.y;
-// 	create_rays(data, data->player.dir);
-// }
+	data->player.pos.x += new_pos.x;
+	data->player.pos.y += new_pos.y;
+	create_board_img(data);
+printf("mbx: %d\n", data->player.pos.x);
+printf("mby: %d\n", data->player.pos.y);
+printf("angle aft: %f\n", data->player.angle * 180 / M_PI);
+	create_cone_multi_rays(data, data->player.angle);
+	// create_rays(data, data->player.view_dst_pos, data->player.angle);
+}
