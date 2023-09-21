7795c0330d5f3e42b613baf7123a460b22afa528
Roro <rolando.rebois@yahoo.fr>
2023-09-21 10:32:21 +0200

    Vision on right side + starting point correct

--- Incs/cub3D.h
+++ Incs/cub3D.h
@@ -6,7 +6,7 @@
 /*   By: rrebois <rrebois@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/04 09:17:33 by tgellon           #+#    #+#             */
-/*   Updated: 2023/09/20 16:10:14 by rrebois          ###   ########lyon.fr   */
+/*   Updated: 2023/09/21 10:10:06 by rrebois          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,7 +51,7 @@
 # define WIN_LEN 720
 # define FOV 60
 # define RAY_NUMBER 180
-# define MOVE_SPEED 2
+# define MOVE_SPEED 0.05
 # define ANGLE_MOVE 0.01
 
 // strings
@@ -93,8 +93,8 @@ typedef struct s_texture
 
 typedef struct s_coord_d
 {
-	int	x;
-	int	y;
+	double	x;
+	double	y;
 }				t_coord_d;
 
 typedef struct s_coord_f
@@ -168,7 +168,7 @@ typedef struct s_col
 	t_coord_f	side_d;
 	int			side_touched;
 	t_coord_f	delta_d;
-	t_coord_d	cell;
+	// t_coord_d	cell; not used
 }			t_col;
 
 typedef struct s_mini
