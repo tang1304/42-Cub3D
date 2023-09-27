5ad8a65adc20a0a40d32ce4d7a5fb3e8ccd3cc97
Rolando Rebois <rrebois@z3r3p3.42lyon.fr>
2023-09-27 09:42:52 +0200

    rays' cone implemented and collision of rays working ok (so it seems)

--- Srcs/rays.c
+++ Srcs/rays.c
@@ -1,30 +1,17 @@
 #include "../Incs/cub3D.h"
 
-void	create_cone_multi_rays(t_data *data, t_coord_d left, t_coord_d right)
+static double	calculate_len_sq_vector(t_data *data, t_coord_f hit)
 {
-	double		inc;(void)inc;
-	int			i;
+	double	len;
 
-	inc = 1.0f / (RAY_NUMBER - 1.0f);
-	i = 0;
-	while (i < RAY_NUMBER)
-	{
-		data->ray[i].hit_p.x = left.x - i * fabs(left.x - right.x) / RAY_NUMBER;
-		data->ray[i].hit_p.y = left.x - i * fabs(left.y - right.y) / RAY_NUMBER;
-
-		t_coord_f	test;
-		test.x = data->ray[i].hit_p.x;
-		test.y = data->ray[i].hit_p.y;
-		create_line(data, test);
-		i++;
-	}
-	// rays_render(data);
-	// mlx_put_image_to_window(data->mlx, data->win, data->img.img, 0, 0);
+	len = (hit.x - data->player.pos.x) * (hit.x - data->player.pos.x) + \
+				(hit.y - data->player.pos.y) * (hit.y - data->player.pos.y);
+	return (len);
 }
 
-static t_coord_d	determine_dst_coord(t_coord_f pos, double angle, int dist)
+static t_coord_f	get_dst_coord(t_coord_f pos, double angle, int dist)
 {
-	t_coord_d	dest;
+	t_coord_f	dest;
 
 	dest.x = dist * cos(-angle) + pos.x;
 	dest.y = dist * sin(-angle) + pos.y;
@@ -40,42 +27,56 @@ static t_coord_d	determine_dst_coord(t_coord_f pos, double angle, int dist)
 	return (dest);
 }
 
+void	create_cone_multi_rays(t_data *data, t_coord_f left, t_coord_f right)
+{
+	t_coord_f	hit;
+	double		inc;
+	double		ang;
+	int			i;
+
+	inc = 1.0f / (RAY_NUMBER - 1.0f);
+	i = 0;(void)left;
+	while (i < RAY_NUMBER)
+	{
+		data->ray[i].hit_p.x = left.x * inc * i + (1 - (inc * i)) * right.x;
+		data->ray[i].hit_p.y = left.y * inc * i + (1 - (inc * i)) * right.y;
+		hit = init_data_collision(data, &data->ray[i]);
+		if (hit.x != -1 && hit.y != -1)
+		{
+			data->ray[i].hit_p = hit;
+			data->ray[i].len = calculate_len_sq_vector(data, hit);
+		}
+		else
+		{
+			data->ray[i].len = -1;
+			ang = data->player.angle - data->fov / 2 + (data->fov / RAY_NUMBER) * i;
+			data->ray[i].hit_p = get_dst_coord(data->player.pos, ang, VIEW_DIST);
+		}
+		create_line(data, data->ray[i].hit_p);
+		i++;
+	}
+	// rays_render(data);
+	// mlx_put_image_to_window(data->mlx, data->win, data->img.img, 0, 0);
+}
+
 void	create_rays(t_data *data)
 {
 	int	opp_len;
-	t_coord_d	left;
-	t_coord_d	right;
+	t_coord_f	left;
+	t_coord_f	right;
 	t_coord_f	position;
 
-	data->player.view_dst_pos = determine_dst_coord(data->player.pos, data->player.angle, VIEW_DIST);
+	position = get_dst_coord(data->player.pos, data->player.angle, VIEW_DIST);
+	data->player.view_dst_pos.x = position.x;
+	data->player.view_dst_pos.y = position.y;
 	opp_len = tan(data->fov / 2) * VIEW_DIST;
-	position.x = data->player.view_dst_pos.x;
-	position.y = data->player.view_dst_pos.y;
-	left = determine_dst_coord(position, data->player.angle + M_PI / 2, opp_len);
-	right = determine_dst_coord(position, data->player.angle - M_PI / 2, opp_len);
+	left = get_dst_coord(position, data->player.angle + M_PI / 2, opp_len);
+	right = get_dst_coord(position, data->player.angle - M_PI / 2, opp_len);
+
 draw_point(data, data->player.view_dst_pos.x, data->player.view_dst_pos.y, RED);
 draw_point(data, left.x, left.y, GREEN);
 draw_point(data, right.x, right.y, BLUE);
 mlx_put_image_to_window(data->mlx, data->win, data->img.img, 0, 0);
 
-/*test*/
-// data->ray[0].hit_p.x = data->player.view_dst_pos.x;
-// 		data->ray[0].hit_p.y = data->player.view_dst_pos.y;
-// create_line(data, &data->ray[0]);
-	create_cone_multi_rays(data, right, left);
-
-
-//test
-// t_coord_f	test;
-// test = init_data_collision(data, &data->ray[0]);
-// // test.x = data->player.view_dst_pos.x;
-// // test.y = data->player.view_dst_pos.y;
-// 	create_line(data, test);//, &data->ray[i]);
-// test.x = left.x;
-// test.y = left.y;
-// 	create_line(data, test);
-// test.x = right.x;
-// test.y = right.y;
-// create_line(data, test);
-
+	create_cone_multi_rays(data, left, right);
 }
