3d683a19f1d0be099ee6e9f64be6c80dc1fe6e5e
Rolando Rebois <rrebois@z3r3p3.42lyon.fr>
2023-10-05 16:21:56 +0200

    update

--- Srcs/textures_extra.c
+++ Srcs/textures_extra.c
@@ -2,7 +2,7 @@
 
 void	load_extra_textures(t_data *data, t_map *map)
 {
-	map->text[4].path = "./textures/door/door.xpm";
+	map->text[4].path = "./textures/door/door3.xpm";
 	map->text[4].text = mlx_xpm_file_to_image(data->mlx, map->text[4].path, \
 			&map->text[4].width, &map->text[4].height);
 	if (!map->text[4].text)
